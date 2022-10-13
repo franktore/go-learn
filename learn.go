1ed85bb0a8be5c31091dae67ba0914f83c49e3bb
fsat <fsat@equinor.com>
2022-10-13 19:27:34 +0200

    copy config files

--- learn.go
+++ learn.go
@@ -72,9 +72,9 @@ func main() {
 
 	// If an error was returned, print it to the console and
 	// exit the program.
-	if err != nil {
-		log.Fatal(err)
-	}
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	// If no error was returned, print the returned message
 	// to the console.
